1f5f933410fd63cd2ae6fce11dbf0f0f33700f4a
Andry Pebrianto <andrypeb227@gmail.com>
2024-02-19 00:50:51 +0700

    Add teleport to modal component

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Transition } from "vue";
+import { Teleport, Transition } from "vue";
 
 defineEmits(["toggle"]);
 defineProps({
@@ -12,27 +12,29 @@ defineProps({
 </script>
 
 <template>
-  <Transition name="modal-outer">
-    <div
-      v-show="modalActive"
-      class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
-    >
-      <Transition name="modal-inner">
-        <div
-          v-if="modalActive"
-          class="p-4 bg-white self-start mt-32 max-w-screen-md"
-        >
-          <slot></slot>
-          <button
-            @click="$emit('toggle')"
-            class="text-white mt-8 bg-[#596FB7] py-2 px-6"
+  <Teleport to="body">
+    <Transition name="modal-outer">
+      <div
+        v-show="modalActive"
+        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+      >
+        <Transition name="modal-inner">
+          <div
+            v-if="modalActive"
+            class="p-4 bg-white self-start mt-32 max-w-screen-md"
           >
-            Close
-          </button>
-        </div>
-      </Transition>
-    </div>
-  </Transition>
+            <slot></slot>
+            <button
+              @click="$emit('toggle')"
+              class="text-white mt-8 bg-[#596FB7] py-2 px-6"
+            >
+              Close
+            </button>
+          </div>
+        </Transition>
+      </div>
+    </Transition>
+  </Teleport>
 </template>
 
 <style scoped>
