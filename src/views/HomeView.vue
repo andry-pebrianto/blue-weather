d895ed6096002b390a4950627f31a6b0767263de
Andry Pebrianto <andrypeb227@gmail.com>
2024-02-20 01:52:21 +0700

    Create redirect when pick an state

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 import axios, { AxiosError } from "axios";
+
+const router = useRouter();
 
 const mapboxApiKey =
   "pk.eyJ1IjoibmlsYWJpcnUiLCJhIjoiY2xzc3JuMWhtMXB0ZTJxbXB5cXd4aDl0aCJ9.LTmkXHL7mu769Tqevu1S3Q";
@@ -34,6 +37,23 @@ const getSearchResults = () => {
     mapboxSearchResults.value = null;
   }, 300);
 };
+
+const previewCity = (searchResult: any) => {
+  const [city, state] = searchResult.place_name.split(",");
+
+  router.push({
+    name: "cityView",
+    params: {
+      state: state.replaceAll(" ", ""),
+      city: city.replaceAll(" ", ""),
+    },
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: 1, // 1 = true, 0 = false
+    },
+  });
+};
 </script>
 
 <template>
@@ -51,7 +71,7 @@ const getSearchResults = () => {
           @input="getSearchResults"
         />
         <ul
-        v-if="mapboxSearchResults"
+          v-if="mapboxSearchResults"
           class="absolute bg-[#718be3] text-white w-full shadow-md py-2 px-1 top-[66px] mt-3 rounded-sm"
         >
           <li
@@ -59,6 +79,7 @@ const getSearchResults = () => {
             v-for="(searchResult, index) in mapboxSearchResults"
             :key="index"
             class="py-2 px-5 mb-1 cursor-pointer hover:bg-[#566fc0]"
+            @click="previewCity(searchResult)"
           >
             {{ searchResult.place_name }}
           </li>
